01e7e9538425e51aa3b72a338c2eca7941290210
Dennismarin08 <a4ntiag0@gmail.com>
2025-07-27 15:51:14 -0500

    Add files via upload

---
+++ data.js
@@ -0,0 +1,18 @@
+const zapatillas = [
+  {
+    nombre: "Nike Air Max 90",
+    precio: 299000,
+    imagen: "zapatillas/zapatilla1.jpg"
+  },
+  {
+    nombre: "Adidas Yeezy Boost",
+    precio: 450000,
+    imagen: "zapatillas/zapatilla2.jpg"
+  },
+  {
+    nombre: "New Balance 574",
+    precio: 210000,
+    imagen: "zapatillas/zapatilla3.jpg"
+  },
+  // Agrega más productos aquí...
+];
