cc2a96277da3ce00a5121358115a5d5aed35b8e6
Dennismarin08 <a4ntiag0@gmail.com>
2025-07-27 21:09:01 -0500

    Add files via upload

---
+++ data.js
@@ -0,0 +1,27 @@
+const zapatillas = [
+  {
+    nombre: "PUMA SUEDE XL",
+    marca: "Puma",
+    precio: 100000,
+    imagen: "zapatillas/PUMA SUEDE XL.jpeg",
+    tallas: [36, 37, 38, 39, 40, 41, 42],
+    genero: "Hombre"
+  },
+  {
+    nombre: "NIKE JORDAN CADENSE  CABALLERO",
+    marca: "Nike",
+    precio: 450000,
+    imagen: "zapatillas/NIKE JORDAN CADENSE  CABALLERO.jpeg",
+    tallas: [38, 39, 40, 41, 42, 43, 44],
+    genero: "Hombre"
+  },
+  {
+    nombre: "New Balance 574",
+    marca: "New Balance",
+    precio: 210000,
+    imagen: "zapatillas/zapatilla3.jpg",
+    tallas: [35, 36, 37, 38, 39, 40],
+    genero: "Hombre"
+  },
+  // Agrega más productos aquí...
+];
