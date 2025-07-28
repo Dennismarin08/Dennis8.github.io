c28b047467a0f179a5d712b47dc32339e296b25d
Dennismarin08 <a4ntiag0@gmail.com>
2025-07-27 22:59:44 -0500

    Add files via upload

---
+++ data.js
@@ -0,0 +1,196 @@
+//catalogo de zapatillas CAMBIAR CATALOGO CADA SEMANA
+
+const zapatillas = [
+  {
+    nombre: "PUMA SUEDE XL",
+    marca: "Puma",
+    precio: 100000,
+    imagen: "zapatillas/PUMA SUEDE XL.jpeg",
+    tallas: [36, 37, 38, 39, 40, 41, 42],
+    genero: "Hombre"
+  },
+  {
+    nombre: "NIKE JORDAN CADENSE  CABALLERO",
+    marca: "Nike",
+    precio: 110000,
+    imagen: "zapatillas/NIKE JORDAN CADENSE  CABALLERO.jpeg",
+    tallas: [38, 39, 40, 41, 42, 43, 44],
+    genero: "Hombre"
+  },
+  {
+    nombre: "NIKE AIR FORCÉ ONE BOTA",
+    marca: "Nike",
+    precio: 115000,
+    imagen: "zapatillas/NIKE AIR FORCÉ ONE BOTA  MUJER Y HOMBRE.jpeg",
+    tallas: [35, 36, 37, 38, 39, 40, 41, 42, 43, 44],
+    genero: "Unisex"
+  },
+  {
+  nombre: "NIKE R1 PARIS ",
+  marca: "Nike",
+  precio:100000,
+  imagen: "zapatillas/NIKE R1 PARIS.jpeg",
+  tallas: [35, 36, 37, 38, 39, 40, 41, 42, 43, 44],
+  genero: "Unisex"
+},
+  {
+  nombre: "NIKE LEGACY",
+  marca: "Nike",
+  precio: 70000,
+  imagen: "zapatillas/NIKE LEGACY NEGRO.jpeg",
+  tallas: [35, 36, 37, 38],
+  genero: "Mujer",
+  estilos: [
+    {
+      nombre: "Negro",
+      imagen: "zapatillas/NIKE LEGACY NEGRO.jpeg"
+    },
+    {
+      nombre: "Blanco",
+      imagen: "zapatillas/NIKE LEGACY BLANCO.jpeg"
+    }
+  ]
+},
+  {
+  nombre: "HUGO BOSS ",
+  marca: "Hugo Boss",
+  precio: 0,
+  imagen: "zapatillas/HUGO BOSS.jpeg",
+  tallas: [],
+  genero: "Hombre"
+},
+  {
+  nombre: "NIKE KYRIE ",
+  marca: "Nike",
+  precio:125000,
+  imagen: "zapatillas/NIKE KYRIE.jpeg",
+  tallas: [],
+  genero: "Mujer"
+},
+  {
+  nombre: "NIKE RETRO 4 RAYO",
+  marca: "Nike",
+  precio: 90000,
+  imagen: "zapatillas/NIKE RETRO 4 RAYO.jpeg",
+  tallas: [],
+  genero: "unisex"
+},
+  {
+  nombre: "ADIDAS SAMBA",
+  marca: "Adidas",
+  precio: 95000,
+  imagen: "zapatillas/ADIDAS SAMBA.jpeg",
+  tallas: [],
+  genero: "Mujer"
+},
+  {
+  nombre: "ADIDAS NASTASE",
+  marca: "Adidas",
+  precio: 120000,
+  imagen: "zapatillas/ADIDAS NASTASE.jpeg",
+  tallas: [],
+  genero: "Hombre"
+},
+  {
+  nombre: "NIKE CORTEZ",
+  marca: "Nike",
+  precio:120000,
+  imagen: "zapatillas/NIKE CORTEZ.jpeg",
+  tallas: [],
+  genero: "Unisex"
+},
+  {
+  nombre: "NIKE ARAÑA NEGRO",
+  marca: "Nike",
+  precio: 115000,
+  imagen: "zapatillas/NIKE ARAÑA NEGRO.jpeg",
+  tallas: [],
+  genero: "Hombre"
+},
+  {
+  nombre: "NIKE ARAÑA AZUL",
+  marca: "Nike",
+  precio: 115000,
+  imagen: "zapatillas/NIKE ARAÑA AZUL.jpeg",
+  tallas: [],
+  genero: "Hombre"
+},
+  {
+  nombre: "NEW BALANCE GRIS",
+  marca: "New Balance",
+  precio: 115000,
+  imagen: "zapatillas/NEW BALANCE GRIS.jpeg",
+  tallas: [],
+  genero: "Hombre"
+},
+  {
+  nombre: "NEW BALANCE NEGRO",
+  marca: "New Balance",
+  precio: 115000,
+  imagen: "zapatillas/NEW BALANCE NEGRO.jpeg",
+  tallas: [],
+  genero: "Hombre"
+},
+  {
+  nombre: "ADIDAS LEGO",
+  marca: "Adidas",
+  precio: 80000,
+  imagen: "zapatillas/ADIDAS LEGO.jpeg",
+  tallas: [],
+  genero: "Hombre"
+},
+
+  {
+  nombre: "NEW BALANCE 999 ",
+  marca: "New Balance",
+  precio: 120000,
+  imagen: "zapatillas/NEW BALANCE 999 BLANCO.jpeg",
+  tallas: [35, 36, 37, 38],
+  genero: "Mujer",
+  estilos: [
+    {
+      nombre: "Negro",
+      imagen: "zapatillas/NEW BALANCE 999 GRIS.jpeg"
+    },
+    {
+      nombre: "Blanco",
+      imagen: "zapatillas/NEW BALANCE 999 BLANCO.jpeg"
+    }
+  ]
+},
+  {
+  nombre: "NIKE TN",
+  marca: "Nike",
+  precio:100000,
+  imagen: "zapatillas/NIKE TN ROJO BLANCO.jpeg",
+  tallas: [],
+  genero: "Hombre",
+  estilos: [
+    {
+      nombre: "Negro",
+      imagen: "zapatillas/NIKE TN ROJO BLANCO.jpeg"
+    },
+    {
+      nombre: "Blanco",
+      imagen: "zapatillas/NIKE NARANJA.jpeg"
+    }
+  ]
+},
+  {
+  nombre: "NIKE JORDAN RETRO 1",
+  marca: "Nike",
+  precio:115000,
+  imagen: "zapatillas/NIKE JORDAN RETRO 1.jpeg",
+  tallas: [],
+  genero: "Mujer",
+},
+  {
+  nombre: "NIKE AIR FORCE ONE",
+  marca: "Nike",
+  precio:100000,
+  imagen: "zapatillas/NIKE AIR FORCE ONE.jpeg",
+  tallas: [],
+  genero: "Unisex"
+},
+
+];
